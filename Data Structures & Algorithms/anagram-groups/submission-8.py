e19a3c4618d3e903f30639f6cca5ba5d6ae69d9f
Syed Hassan Ali Rizvi <59576232+HassanAliRizvi@users.noreply.github.com>
2026-09-05 13:19:15 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for word in strs:
+            freq = [0] * 26
+            for char in word:
+                freq[ord('a') - ord(char)] += 1
+            if tuple(freq) in groups:
+                groups[tuple(freq)].append(word)
+            else:
+                groups[tuple(freq)] = [word]
+        
+        return list(groups.values())
+
+        # space = O(m)
+        # time = O(n) * O(m log m)
+            
+
+
+        
