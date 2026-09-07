bad5bada223e7478bec801ecc733d7ca2d02743a
Syed Hassan Ali Rizvi <59576232+HassanAliRizvi@users.noreply.github.com>
2026-09-07 13:52:15 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        # how can I make sure the item I popped from self.stack is also popped from min_stack
+        item = self.stack.pop()
+        if item == self.min_stack[-1]:
+            self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
